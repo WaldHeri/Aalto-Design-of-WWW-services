adf84156c23df83fe66a6746cfdf7dcdd45a3c64
Ville Vuorenmaa <villevuor@gmail.com>
2019-11-11 13:50:05 +0200

    Remove twig

--- web/index.php
+++ web/index.php
@@ -1,39 +1,8 @@
-<?php
+<?php require('functions.php'); ?>
+<?php get_header(); ?>
 
-require('../vendor/autoload.php');
+<h1>Link Scrapbook</h1>
 
-$app = new Silex\Application();
-$app['debug'] = true;
+<p>blablabla</p>
 
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
-));
-
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
-
-// Our web handlers
-
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
-});
-
-$app->run();
-
-$dbopts = parse_url(getenv('DATABASE_URL'));
-$app->register(new Csanquer\Silex\PdoServiceProvider\Provider\PDOServiceProvider('pdo'),
-  array(
-  'pdo.server' => array(
-      'driver'   => 'pgsql',
-      'user' => $dbopts["user"],
-      'password' => $dbopts["pass"],
-      'host' => $dbopts["host"],
-      'port' => $dbopts["port"],
-      'dbname' => ltrim($dbopts["path"],'/')
-      )
-  )
-);
+<?php get_footer(); ?>
