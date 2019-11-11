40ebbf03c78dd9201c79efb537a97dabd7baf46d
Ville Vuorenmaa <villevuor@gmail.com>
2019-11-11 13:53:58 +0200

    Two sample pages and updated nav

---
+++ web/about.php
@@ -0,0 +1,8 @@
+<?php require('functions.php'); ?>
+<?php get_header('About Link Scrapbook'); ?>
+
+<h1>About Link Scrapbook</h1>
+
+<p>blablabla</p>
+
+<?php get_footer(); ?>
---
+++ web/my_scrapbooks.php
@@ -0,0 +1,8 @@
+<?php require('functions.php'); ?>
+<?php get_header('My scrapbooks'); ?>
+
+<h1>My scrapbooks</h1>
+
+<p>blablabla</p>
+
+<?php get_footer(); ?>
