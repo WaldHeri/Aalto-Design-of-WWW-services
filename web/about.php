275520db3a3025827759933b2d205cdf4c2eb6d0
sonianefertiti <56894209+sonianefertiti@users.noreply.github.com>
2019-11-13 15:43:58 +0200

    Etusivupäivitys

--- web/about.php
+++ web/about.php
@@ -3,6 +3,10 @@
 
 <h1>About Link Scrapbook</h1>
 
-<p>moiccu moi</p>
+<p>Link Scrapbook is a service founded in 2019. </p>
+
+<h3>Why do I need yet another service to use?</h3>
+
+
 
 <?php get_footer(); ?>
