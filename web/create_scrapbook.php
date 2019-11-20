17b60b769fd7418126e45a5a8c94d4f4a5727f56
Ville Vuorenmaa <villevuor@gmail.com>
2019-11-20 16:08:31 +0200

    Format forms better

--- web/create_scrapbook.php
+++ web/create_scrapbook.php
@@ -5,12 +5,18 @@
 
 <form id="createForm" method="post" action="functions/createScrapbook.func.php" class="col-md-8">
   <h1 class="h1 mb-3 font-weight-normal">Create new scrapbook</h1>
-  <label for="inputTitle" class="sr-only">Scrapbook title</label>
-  <input name="title" type="text" id="inputTitle" class="form-control" placeholder="Scrapbook title" required autofocus>
-  <label for="inputDescription" class="sr-only">Description</label>
-  <textarea name="description" class="form-control" rows="5" id="inputDescription" placeholder="Write your description here" form="createForm"></textarea>
-  <div class="checkbox">
-    <label><input name="public" type="checkbox" id="inputPublicity" value="true"> Public</label>
+  <div class="form-group">
+    <label for="inputTitle">Scrapbook title</label>
+    <input name="title" type="text" id="inputTitle" class="form-control" placeholder="Scrapbook title" required autofocus>
+  </div>
+  <div class="form-group">
+    <label for="inputDescription">Description</label>
+    <textarea name="description" class="form-control" rows="5" id="inputDescription" placeholder="Write your description here" form="createForm"></textarea>
+  </div>
+  <div class="form-group">
+    <div class="checkbox">
+      <label><input name="public" type="checkbox" id="inputPublicity" value="true"> Public</label>
+    </div>
   </div>
   <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit" >Create</button>
 </form>
