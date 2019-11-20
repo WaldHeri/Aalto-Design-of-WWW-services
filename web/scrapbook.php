0bac0d2c42177d2d299cb8d5bff60f6557523844
Ville Vuorenmaa <villevuor@gmail.com>
2019-11-20 16:00:30 +0200

    Show link of public scrapbook to share

--- web/scrapbook.php
+++ web/scrapbook.php
@@ -46,12 +46,20 @@ $scraps = pg_fetch_all($result);
 
 <?php if($is_own_scrapbook) { ?>
   <p><a href="create_scrap.php?id=<?php echo $_GET['id']; ?>" class="btn btn-primary">Add new link</a></p>
+
+  <?php if ($scrapbook['public']) { ?>
+    <div class="alert alert-secondary" role="alert">
+      <p><strong>This scrapbook is public.</strong> Share it to your friends with link below.</p>
+      <input type="text" class="form-control" disabled value="https://linkscrapbook.herokuapp.com/scrapbook.php?id=<?php echo $_GET['id']; ?>">
+    </div>
   <?php } ?>
+<?php } ?>
 
 <div class="row">
   <?php
   if (empty($scraps) && $is_own_scrapbook) {
-    echo '<h4>Your scrapbook is still empty. Add your first link from the button above!</h4>';
+    echo '<div class="alert alert-primary" role="alert">
+    Your scrapbook is still empty. Add your first link from the button above!</div>';
   } else {
     foreach ($scraps as $scrap) {
       echo '<div class="col-md-4">
